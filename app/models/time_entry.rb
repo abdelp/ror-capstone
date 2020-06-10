126877c7bddef4bd62a8cf217d993016aa9cc7e3
abdelp <juniorperezpy@gmail.com>
2020-06-09 20:22:34 -0400

    Add user - time_entry association

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,3 +1,4 @@
 class TimeEntry < ApplicationRecord
   belongs_to :group
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :time_entries, inverse_of: 'author', foreign_key: :author_id
+  has_many :groups
+
+  def my_transactions
+
+  end
 end
