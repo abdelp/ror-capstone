81f7a8a3486d6784c63cd3b602f46b9d388e225e
abdelp <juniorperezpy@gmail.com>
2020-06-11 09:59:36 -0400

    Add cloudinary to upload user profile photo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,35 @@
+require 'digest/sha1'
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
                                         :following, :followers]
-  # GET /users
-  # GET /users.json
+  before_action :check_configuration, only: [:new]
+
+  def check_configuration
+    render 'configuration_missing' if Cloudinary.config.api_key.blank?
+  end
+
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
-  # GET /users/new
   def new
     session[:forwarding_url] = login_path
     @user = User.new
+
     render layout: "logged_out"
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.new(name: user_params[:name])
+    uploaded_file = Cloudinary::Uploader.upload(user_params[:gravatar_url])
+    @user.gravatar_url = uploaded_file['secure_url']
 
     respond_to do |format|
       if @user.save
@@ -40,8 +42,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -54,8 +54,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
@@ -69,13 +67,8 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_user
       @user = User.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :gravatar_url)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :time_entries, inverse_of: 'author', foreign_key: :author_id
   has_many :groups
   validates :name, presence: true, uniqueness: true
+  # mount_uploader :gravatar_url, AvatarUploader
 end
