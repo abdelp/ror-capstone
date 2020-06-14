d4a622d34cf0c5a8b7a5bd3eb3af3b1d7161dc94
abdelp <juniorperezpy@gmail.com>
2020-06-14 19:05:13 -0400

    Remove comments

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,5 +1,5 @@
 class TimeEntry < ApplicationRecord
-  belongs_to :group, required: false # optional
+  belongs_to :group, required: false
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   validates :name, presence: true
   validates :start_time, presence: true
