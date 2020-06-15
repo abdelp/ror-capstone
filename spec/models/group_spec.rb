ee0902bc6b3c522b1f9ad14f61273ac8f8dbd35c
abdelp <juniorperezpy@gmail.com>
2020-06-15 11:16:41 -0400

    Add presence and belonging validators

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should belong_to(:user).required(true) }
 end
