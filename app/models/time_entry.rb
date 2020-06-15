afafe2f5fd1819fc378d238740d8b1055d89a647
abdelp <juniorperezpy@gmail.com>
2020-06-15 11:20:13 -0400

    Change default_scope to use create_at to order

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,10 +1,10 @@
 class TimeEntry < ApplicationRecord
   belongs_to :group, required: false
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id', required: true
   validates :name, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
-  default_scope { order(name: :desc) }
+  default_scope { order(created_at: :desc) }
   validate :no_overlaping
 
   private
@@ -12,6 +12,6 @@ class TimeEntry < ApplicationRecord
   def no_overlaping
     return unless !start_time.nil? && end_time.nil? && start_time < end_time
 
-    errors.add(:end_time, "end_time can't be equal or less than start_time")
+    errors.add(:end_time, "end time can't be equal or less than start_time")
   end
 end
