d0b9b4680c860216620677612565352053a92801
abdelp <juniorperezpy@gmail.com>
2020-06-15 11:17:32 -0400

    Add presence and belonging validators

--- spec/models/time_entry_spec.rb
+++ spec/models/time_entry_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe TimeEntry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:start_time) }
+  it { should validate_presence_of(:end_time) }
+  it { should belong_to(:author).required(true) }
+  it { should belong_to(:group).required(false) }
 end
