a6d9230832b8bae83a751a5879437475ed0df98f
abdelp <juniorperezpy@gmail.com>
2020-06-17 14:31:51 -0400

    Add final Rspec testing cases

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -17,8 +17,53 @@ RSpec.describe User, driver: :selenium_chrome, js: true do
       end
 
       click_button 'Signup'
-      sleep(2)
+      sleep(3)
       expect(page.get_rack_session_key('user_id')).to eq(1)
+    end
+
+    it 'rejects user registration when full name is not specified' do
+      visit signup_path
+
+      within('#new_user') do
+        attach_file('change-img-field', '/tmp/profile.png', make_visible: true)
+        fill_in 'Username', with: user[:name]
+        fill_in 'Email', with: user[:email]
+        fill_in 'Password', with: user[:password]
+      end
+
+      click_button 'Signup'
+      sleep(3)
+      expect(page).to have_content("Full name can't be blank")
+    end
+
+    it 'rejects user registration when name is not specified' do
+      visit signup_path
+
+      within('#new_user') do
+        attach_file('change-img-field', '/tmp/profile.png', make_visible: true)
+        fill_in 'Full name', with: user[:full_name]
+        fill_in 'Email', with: user[:email]
+        fill_in 'Password', with: user[:password]
+      end
+
+      click_button 'Signup'
+      sleep(3)
+      expect(page).to have_content("Name can't be blank")
+    end
+
+    it 'rejects user registration when email is not specified' do
+      visit signup_path
+
+      within('#new_user') do
+        attach_file('change-img-field', '/tmp/profile.png', make_visible: true)
+        fill_in 'Full name', with: user[:full_name]
+        fill_in 'Username', with: user[:name]
+        fill_in 'Password', with: user[:password]
+      end
+
+      click_button 'Signup'
+      sleep(3)
+      expect(page).to have_content("Email can't be blank")
     end
 
     it 'user can login with just username' do
@@ -28,8 +73,28 @@ RSpec.describe User, driver: :selenium_chrome, js: true do
       end
 
       click_button 'Log in'
-      sleep(2)
+      sleep(3)
       expect(page.get_rack_session_key('user_id')).to eq(2)
+    end
+
+    it 'rejects user access when it is not provided a username' do
+      visit login_path
+
+      click_button 'Log in'
+      sleep(3)
+      expect(page).to have_content('Invalid username')
+    end
+
+    it 'rejects user access when username is not registered' do
+      visit login_path
+
+      within('#login_form') do
+        fill_in 'Username', with: 'inexistent_user'
+      end
+
+      click_button 'Log in'
+      sleep(3)
+      expect(page).to have_content('Invalid username')
     end
 
     it 'logout user' do
@@ -38,7 +103,7 @@ RSpec.describe User, driver: :selenium_chrome, js: true do
 
       find('#sidebarCollapse').click
       click_on 'Log out'
-      sleep(2)
+      sleep(3)
       expect { page.get_rack_session_key('user_id') }.to raise_error(KeyError)
     end
   end
--- spec/features/time_entry_spec.rb
+++ spec/features/time_entry_spec.rb
@@ -16,8 +16,67 @@ RSpec.describe TimeEntry, driver: :selenium_chrome, js: true do
       fill_in 'Description', with: time_entry[:name]
       fill_in 'Start time', with: time_entry[:start_time]
       fill_in 'End time', with: time_entry[:end_time]
-      select 'MyString', from: 'time_entry_group_id'
+      select 'mystring', from: 'time_entry_group_id'
+
       click_on 'Create'
+      sleep(3) 
+      expect(page).to have_content 'Time entry was successfully created.'
+    end
+
+    it "rejects from saving time entry when it's not indicated the name" do
+      visit new_time_entry_path
+
+      fill_in 'Start time', with: time_entry[:start_time]
+      fill_in 'End time', with: time_entry[:end_time]
+
+      click_on 'Create'
+      sleep(3) 
+      expect(page).to have_content "Name can't be blank"
+    end
+
+    it "rejects from saving time entry when it's not indicated the starting time" do
+      visit new_time_entry_path
+
+      fill_in 'Description', with: time_entry[:name]
+      fill_in 'End time', with: time_entry[:end_time]
+
+      click_on 'Create'
+      sleep(3) 
+      expect(page).to have_content "Start time can't be blank"
+    end
+
+    it "rejects from saving time entry when it's not indicated the ending time" do
+      visit new_time_entry_path
+
+      fill_in 'Description', with: time_entry[:name]
+      fill_in 'Start time', with: time_entry[:start_time]
+
+      click_on 'Create'
+      sleep(3) 
+      expect(page).to have_content "End time can't be blank"
+    end
+
+    it 'rejects from save when end_time is less than start_time' do
+      visit new_time_entry_path
+
+      fill_in 'Description', with: time_entry[:name]
+      fill_in 'Start time', with: time_entry[:end_time]
+      fill_in 'End time', with: time_entry[:start_time]
+
+      click_on 'Create'
+      sleep(3)
+      expect(page).to have_content "end time can't be equal or less than start_time"
+    end
+
+    it 'saves even when no group is selected' do
+      visit new_time_entry_path
+
+      fill_in 'Description', with: time_entry[:name]
+      fill_in 'Start time', with: time_entry[:start_time]
+      fill_in 'End time', with: time_entry[:end_time]
+
+      click_on 'Create'
+      sleep(3)
       expect(page).to have_content 'Time entry was successfully created.'
     end
   end
