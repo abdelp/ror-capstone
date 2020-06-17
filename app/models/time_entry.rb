4da5735b5fdeb7fd7bb17057b5f9260b806a59e4
abdelp <juniorperezpy@gmail.com>
2020-06-17 13:09:41 -0400

    Fix no_overlaping scope

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,7 +1,7 @@
 class TimeEntry < ApplicationRecord
   belongs_to :group, required: false
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', required: true
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
   validates :start_time, presence: true
   validates :end_time, presence: true
   default_scope { order(created_at: :desc) }
@@ -10,7 +10,7 @@ class TimeEntry < ApplicationRecord
   private
 
   def no_overlaping
-    return unless !start_time.nil? && end_time.nil? && start_time < end_time
+    return if !start_time.nil? && !end_time.nil? && start_time < end_time
 
     errors.add(:end_time, "end time can't be equal or less than start_time")
   end
