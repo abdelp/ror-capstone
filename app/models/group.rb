0e32ad821a91c4dbdf7d26ec7f6168e6f3416327
abdelp <juniorperezpy@gmail.com>
2020-06-17 12:20:55 -0400

    Add max length

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,13 @@
 class Group < ApplicationRecord
   belongs_to :user, required: true
   has_many :time_entries
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
   default_scope { order(name: :asc) }
+  before_save :downcase_name
+
+  private
+
+  def downcase_name
+    self.name = name.downcase
+  end
 end
