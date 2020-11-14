2c1dcdaa978ee49fffa42762a2edef77a035545f
abdelp <juniorperezpy@gmail.com>
2020-11-14 19:54:56 -0300

    Comment manual session helpers to replace for devise helpers

--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -3,30 +3,30 @@ module UserSessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user?(user)
-    user == current_user
-  end
+  # def current_user?(user)
+  #   user == current_user
+  # end
 
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user
-        log_in user
-        @current_user = user
-      end
-    end
-  end
+  # def current_user
+  #   if (user_id = session[:user_id])
+  #     @current_user ||= User.find_by(id: user_id)
+  #   elsif (user_id = cookies.signed[:user_id])
+  #     user = User.find_by(id: user_id)
+  #     if user
+  #       log_in user
+  #       @current_user = user
+  #     end
+  #   end
+  # end
 
-  def logged_in?
-    !current_user.nil?
-  end
+  # def logged_in?
+  #   !current_user.nil?
+  # end
 
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
+  # def log_out
+  #   session.delete(:user_id)
+  #   @current_user = nil
+  # end
 
   def redirect_back_or(default)
     redirect_to(session[:forwarding_url] || default)
