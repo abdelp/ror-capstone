dd50a3f284685e1abf6d6e01bea3e0fff883481c
abdelp <juniorperezpy@gmail.com>
2020-11-14 19:55:41 -0300

    Update email validation and remove has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,7 @@ class User < ApplicationRecord
   has_many :groups, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
   before_save { self.email = email.downcase }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   validates :password, presence: true, length: { minimum: 6 }
-  has_secure_password
 end
