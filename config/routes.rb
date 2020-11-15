a5b444dc1868c2d10707e3dfcaf151c3819394ba
abdelp <juniorperezpy@gmail.com>
2020-11-15 14:54:53 -0300

    Update logout route to devise's route to logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: {
       sessions: 'users/sessions',
-      registrations: 'users/registrations',
+      registrations: 'users/registrations'
     },
     path: 'auth'
     # ,
