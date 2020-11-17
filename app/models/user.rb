246abd986e6697455a82f519a24110ca3c5b9460
abdelp <juniorperezpy@gmail.com>
2020-11-17 19:44:53 -0300

    Update user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  include Rails.application.routes.url_helpers
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,7 +8,6 @@ class User < ApplicationRecord
   has_many :groups, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
   before_save { self.email = email.downcase }
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
 
-  validates :password, presence: true, length: { minimum: 6 }
+  has_one_attached :avatar
 end
