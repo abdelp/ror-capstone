07d0ecb72510fd2ed3d2ebbe2edc722ba39ce08f
abdelp <juniorperezpy@gmail.com>
2020-11-26 12:56:40 -0300

    Implement omniauth callback method for facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,22 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   layout 'logged_out'
+  skip_before_action :verify_authenticity_token, only: :facebook
+ 
+  def facebook
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra)
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
 
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
