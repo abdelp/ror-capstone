9f7d7f8a785ef6b75685f8bc22980c74f94a3f84
abdelp <juniorperezpy@gmail.com>
2020-11-28 16:11:37 -0300

    Add devise for omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -157,7 +157,7 @@ Devise.setup do |config|
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
-  config.reconfirmable = true
+  config.reconfirmable = false
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]
@@ -272,6 +272,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, ENV['facebook_app_id'], ENV['facebook_app_secret']
+
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
