9f6e46db0fc06c7280a1aab49f6cf791a73f7323
abdelp <juniorperezpy@gmail.com>
2020-11-30 14:55:30 -0300

    Update double quotes to single quotes

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,15 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   layout 'logged_out'
   skip_before_action :verify_authenticity_token, only: :facebook
- 
+
   def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra)
+      session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
       redirect_to new_user_registration_url
     end
   end
