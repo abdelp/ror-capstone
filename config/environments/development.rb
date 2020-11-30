7f676bcfd509419526e6acd473976f659f378410
abdelp <juniorperezpy@gmail.com>
2020-11-30 15:02:57 -0300

    Disable email delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,11 +61,11 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = false
-  # # config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default :charset => "utf-8"
 
   # config.action_mailer.smtp_settings = {
   #   :address   => "smtp.mandrillapp.com",
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,8 +113,8 @@ Rails.application.configure do
   # Devise
   config.action_mailer.default_url_options = { host: 'https://whispering-retreat-92662.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = false
   # config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
