24daf1bb1fb297d94b664291e272aa6055df901e
abdelp <juniorperezpy@gmail.com>
2020-11-30 18:06:29 -0300

    Change environment credential names for sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     :address   => "smtp.sendgrid.net",
     :port      => 587,
     :authentication => :plain,
-    :user_name => ENV['sendgrid_username'],
+    :user_name => ENV['SENDGRID_USERNAME'],
     :password  => ENV['SENDGRID_API_KEY'],
     :enable_starttls_auto => true
   }
